ff6aff01e969c23beb867d9545c7c90d59bd4e3b
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-06-09 01:24:29 -0700

    Update lists.py

--- lists.py
+++ lists.py
@@ -4,3 +4,11 @@ print(fruits_set)
 #explore type and length
 type(fruits_set)
 len(fruits_set)
+# many sets
+set1 = {"banana", "lemon", "apple"}
+set2 = {1, 2, 3, 4}
+set3 = {False, True, False}
+
+print(set1)
+print(set2)
+print(set3)
